68e5f89593623c9ee66b70ce55622b8e7913b94c
alexanderdarren <darren.satria23@gmail.com>
2023-06-04 21:12:50 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+const hamburger = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
